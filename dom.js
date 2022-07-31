936f1df97f920ba50c20a1049d70e7aaa301fc87
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-07-31 18:16:16 +0530

    third commit

--- dom.js
+++ dom.js
@@ -18,3 +18,12 @@ items[3].style.backgroundColor = 'lightblue'
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight = 'bold'
 }
+
+
+
+var liitem = document.getElementsByClassName('item5')
+liitem[0].style.fontWeight = 'bold'
+liitem[0].style.backgroundColor = 'pink'
+
+var li5 = document.getElementsByTagName('li')
+li5[4].style.backgroundColor = 'pink'
