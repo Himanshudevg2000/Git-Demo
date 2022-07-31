310e085a6519c137be5be99c0433e344654eafc1
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-07-31 17:58:54 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,20 @@
+
+
+var header = document.getElementById('main-header')
+header.style.borderBottom = 'solid 3px #000'
+
+var addItems = document.getElementsByClassName('title')
+console.log(addItems)
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+
+addItems[0].style.fontWeight = 'bold'
+items[0].style.backgroundColor = 'yellow'
+items[1].style.backgroundColor = 'red'
+items[2].style.backgroundColor = 'green'
+items[3].style.backgroundColor = 'lightblue'
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold'
+}
