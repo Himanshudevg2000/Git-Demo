f228db75752ef587b45d374f2339ae8afca9760a
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-08-05 18:46:02 +0530

    seventh commit

--- dom.js
+++ dom.js
@@ -64,3 +64,81 @@ let secondText = document.createTextNode('HEllo');
 let listgroup = document.querySelector('.list-group')
 let li = document.querySelector('li')
 listgroup.insertBefore(secondText,li)
+
+
+
+// task 8
+let form = document.getElementById('addForm')
+
+let itemlist = document.getElementById('items')
+
+
+
+form.addEventListener('submit', addItem)
+
+itemlist.addEventListener('click',removeItem)
+
+
+
+function addItem(e){
+
+    e.preventDefault();
+
+
+
+    let newItem = document.getElementById('item').value
+
+
+
+    let li = document.createElement('li')
+
+    li.className = 'list-group-item'
+
+    li.appendChild(document.createTextNode(newItem))
+
+
+
+    let editbtn = document.createElement('button');
+
+    editbtn.className = 'btn btn-sm float-right';
+
+    editbtn.appendChild(document.createTextNode('EDIT'))
+
+    let deleteBtn = document.createElement('button')
+
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
+
+    deleteBtn.appendChild(document.createTextNode('X'))
+
+    
+
+    li.appendChild(deleteBtn)
+
+    itemlist.appendChild(li)
+
+    
+
+    li.appendChild(editbtn)
+
+    itemlist.appendChild(li)
+
+}
+
+
+
+function removeItem(e){
+
+    if(e.target.classList.contains('delete')){
+
+        if(confirm('Are you sure?')){
+
+            let li = e.target.parentElement
+
+            itemlist.removeChild(li)
+
+        }
+
+    }
+
+}
+
