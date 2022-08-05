154a1ba8e2091b14b098920c5f70b479002a89f7
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-08-05 18:48:00 +0530

    eight commit

--- dom.js
+++ dom.js
@@ -142,3 +142,20 @@ function removeItem(e){
 
 }
 
+
+// task 9
+
+function filterItems(e){
+    var text =e.target.value.toLowerCase();
+    var items = itemlist.getElementsByTagName('li')
+   Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 ){
+        item.style.display = 'block';
+    }else {
+        item.style.display = 'none';
+    }
+
+   });
+
+}
